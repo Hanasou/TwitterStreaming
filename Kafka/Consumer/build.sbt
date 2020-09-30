f90c37cde681da1f586ad536236b5c940de160e7
Hanasou <royzhang42@gmail.com>
2020-09-30 10:40:09 -0700

    twitter consumer

--- Kafka/Consumer/build.sbt
+++ Kafka/Consumer/build.sbt
@@ -3,3 +3,10 @@ name := "Consumer"
 version := "0.1"
 
 scalaVersion := "2.12.12"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-streaming
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "3.0.1" % "provided"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-streaming-kafka-0-10
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "3.0.1"
+
+
---
+++ Kafka/Consumer/src/main/scala/consumer/TwitterConsumer.scala
@@ -0,0 +1,64 @@
+package consumer
+
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+
+object TwitterConsumer {
+
+  def createStream(ssc: StreamingContext, bootstrapServers: String, groupId: String, topic: String):
+  InputDStream[ConsumerRecord[String, String]] = {
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> bootstrapServers,
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> groupId,
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> "false",
+      "max.poll.records" -> "100"
+    )
+    val topics = Array(topic)
+
+    val twitterStream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+    // This returns a Stream of ConsumerRecords
+    // Each ConsumerRecord contains a key and a value
+    twitterStream
+  }
+
+  def extractText(jsonString: String): String = {
+    null
+  }
+
+  def run(): Unit = {
+    // Initialize streaming context
+    val ssc = new StreamingContext("local[*]", "TwitterConsumer", Seconds(1))
+
+    val bootstrapServers = "localhost:9092"
+    val groupId = "twitter_group"
+    val topic = "twitter_topic"
+
+    val tweetStream = createStream(ssc, bootstrapServers, groupId, topic)
+    val keysAndValues = tweetStream.map(record => (record.key, record.value))
+    keysAndValues.print()
+
+    val checkpointDir = "C:/checkpoint/"
+    ssc.checkpoint(checkpointDir)
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  def main(args: Array[String]): Unit = {
+    run()
+  }
+}
